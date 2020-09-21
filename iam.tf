3b44b4de1781c05ec1ba2b2320a5c89b0e0d21b5
Ondřej Tůma <ondrej.tuma@lukapo.com>
2020-09-21 11:59:19 +0200

    feat: add rw option

--- iam.tf
+++ iam.tf
@@ -91,9 +91,29 @@ data "aws_iam_policy_document" "another-accounts-access" {
       type        = "AWS"
     }
     actions = [
-      "ecr:BatchGetImage",
-      "ecr:DescribeImages",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
       "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:GetLifecyclePolicy",
+      "ecr:GetLifecyclePolicyPreview",
+      "ecr:ListTagsForResource",
+      "ecr:DescribeImageScanFindings",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage",
+      "ecs:List*",
+      "ecs:Describe*",
+      "ecs:CreateService",
+      "ecs:RegisterTaskDefinition",
+      "iam:PassRole",
+      "ecs:UpdateService",
+      "ecs:DeregisterTaskDefinition",
       "ecr:GetDownloadUrlForLayer"
     ]
 
